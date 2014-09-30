e95b5f0d69a7c4f6bb0535cffd1edbc1437d5d83
Gerrit <treevg@gmail.com>
2014-09-30 17:18:16 +0200

    initial cmake framework

---
+++ src/cmake/FindGLEW.cmake
@@ -0,0 +1,44 @@
+#
+# Try to find GLEW library and include path.
+# Once done this will define
+#
+# GLEW_FOUND
+# GLEW_INCLUDE_PATH
+# GLEW_LIBRARY
+# 
+
+set(OpenGL_ROOT_ENV $ENV{OpenGL_ROOT})
+
+IF (WIN32)
+	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+		${OpenGL_ROOT_ENV}/include/
+	)
+
+    FIND_LIBRARY( GLEW_LIBRARY
+        NAMES glew32s.lib
+        PATHS ${OpenGL_ROOT_ENV}/lib
+    )
+    
+
+ELSEIF(APPLE)
+	SET(GLEW_INCLUDE_PATH 
+	   $ENV{OpenGL_ROOT}/include/)
+	 
+	SET(GLEW_LIBRARY
+	   $ENV{OpenGL_ROOT}/include/libGLEW.a)
+
+ELSE()
+	FIND_PATH(GLEW_INCLUDE_PATH GL/glew.h)
+	FIND_LIBRARY(GLEW_LIBRARY
+	NAMES GLEW glew32 glew glew32s PATH_SUFFIXES lib64)
+ENDIF()
+    
+
+SET(GLEW_FOUND "NO")
+IF (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
+	SET(GLEW_LIBRARIES ${GLEW_LIBRARY})
+	SET(GLEW_FOUND "YES")
+    message("EXTERNAL LIBRARY 'GLEW' FOUND")
+ELSE()
+    message("ERROR: EXTERNAL LIBRARY 'GLEW' NOT FOUND")
+ENDIF (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
---
+++ src/cmake/FindGLFW3.cmake
@@ -0,0 +1,58 @@
+#
+# Try to find GLFW3 library and include path.
+# Once done this will define
+#
+# GLFW3_FOUND
+# GLFW3_INCLUDE_PATH
+# GLFW3_LIBRARY
+# 
+
+set(GLFW3_ROOT_ENV $ENV{GLFW3_ROOT})
+
+IF (MINGW)
+    FIND_PATH( GLFW3_INCLUDE_PATH GLFW/glfw3.h
+        ${GLFW3_ROOT_ENV}/include
+    )
+
+    FIND_LIBRARY( GLFW3_LIBRARY
+        NAMES glfw3
+        PATHS
+        ${GLFW3_ROOT_ENV}/lib-mingw
+    )
+
+ELSEIF (MSVC)
+    FIND_PATH( GLFW3_INCLUDE_PATH GLFW/glfw3.h
+        ${GLFW3_ROOT_ENV}/include
+    )
+
+    FIND_LIBRARY( GLFW3_LIBRARY
+        NAMES glfw3
+        PATHS
+        ${GLFW3_ROOT_ENV}/lib-msvc110
+    )
+ELSEIF(APPLE)
+
+    FIND_PATH(GLFW3_INCLUDE_PATH GLFW/glfw3.h
+    ${GLFW3_ROOT_ENV}/include)
+    
+    FIND_LIBRARY( GLFW3_LIBRARY
+        NAMES libglfw3.a
+        PATHS $ENV{GLFW3_ROOT_ENV}/build/src)
+
+ELSE()
+	FIND_PATH(GLFW3_INCLUDE_PATH GLFW/glfw3.h)
+	FIND_LIBRARY(GLFW3_LIBRARY
+        NAMES glfw3 glfw
+	PATH_SUFFIXES dynamic) 
+ENDIF ()
+
+
+
+SET(GLFW3_FOUND "NO")
+IF (GLFW3_INCLUDE_PATH AND GLFW3_LIBRARY)
+	SET(GLFW3_LIBRARIES ${GLFW3_LIBRARY})
+	SET(GLFW3_FOUND "YES")
+    message("EXTERNAL LIBRARY 'GLFW3' FOUND")
+ELSE()
+    message("ERROR: EXTERNAL LIBRARY 'GLFW3' NOT FOUND")
+ENDIF (GLFW3_INCLUDE_PATH AND GLFW3_LIBRARY)
