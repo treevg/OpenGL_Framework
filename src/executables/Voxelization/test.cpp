237dd177aeb197c83f7f12cb231ab1610eb6626e
Secumfex <Arend.Buchacher@gmx.de>
2014-11-25 11:18:16 +0100

    Slicemapping - Bitmask WIP
    - added bitmask method
    - unknown behaviour on upload, since texture is 1D, but method uploads
    2D texture

--- src/executables/Voxelization/test.cpp
+++ src/executables/Voxelization/test.cpp
@@ -4,6 +4,32 @@
 
 #define PI 3.14159265359f
 
+// create 32bit uint bitmask
+GLuint createBitMask()
+{
+	GLuint bitmask = 0;
+
+	// 32 bit values, one bit set per entry
+	unsigned long int bitmaskData[32] =
+			{ 1u, 2u, 4u, 8u, 16u, 32u, 64u, 128u,
+			256u, 512u, 1024u, 2048u, 4096u, 8192u, 16384u, 32768u, 65536u,
+			131072u, 262144u, 524288u, 1048576u, 2097152u, 4194304u, 8388608u,
+			16777216u, 33554432u, 67108864u, 134217728u, 268435456u, 536870912u,
+			1073741824u, 2147483648u };
+
+	glGenTextures(1, &bitmask);
+	glBindTexture(GL_TEXTURE_1D, bitmask);
+
+	// allocate memory:  1D Texture,  1 level,   long uint format (32bit)
+	glTexStorage1D( GL_TEXTURE_1D, 1, GL_R32UI, 32);
+	glTexSubImage1D( GL_TEXTURE_1D, 0, 0, 32, GL_RED_INTEGER, GL_UNSIGNED_INT, &bitmaskData);
+	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glBindTexture(GL_TEXTURE_1D, 0);
+
+	return bitmask;
+}
+
 auto sp = new ShaderProgram({"/Voxelization/simpleVertex.vert", "/Voxelization/simpleColoring.frag"});
 auto slicemappingShader = new ShaderProgram({"/Voxelization/simpleVertex.vert", "/Voxelization/sliceMapMultipleTargets.frag"});
 
@@ -19,7 +45,7 @@ auto slicemappingPass = new RenderPass(
     slicemappingShader
 );
 
-GLuint bitmask;
+GLuint bitmask = createBitMask();
 
 float near = 0.1f;
 float far  = 6.0f;
@@ -28,24 +54,16 @@ float red = 0.0f;
 float green = 1.0f;
 float blue = 0.0f;
 
-
 glm::mat4 model = glm::mat4(1.0f);
 glm::mat4 view = glm::lookAt(glm::vec3(0.0f,0.0f,3.0f), glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,1.0f,0.0f) );
 glm::mat4 projection = glm::perspective(60.0f * PI / 180.0f, (float) width/height, near, far );
-
-GLuint createBitMask()
-{
-	//TODO
-	return 0;
-}
 
 int main(int argc, char *argv[]) {
     sp -> printUniformInfo();
     sp -> printInputInfo();
     sp -> printOutputInfo();
 
-    //create bitmask
-    bitmask = createBitMask();
+    //TODO framebuffers
 
     renderLoop([]{
 
@@ -63,7 +81,7 @@ int main(int argc, char *argv[]) {
 		->update("near", near)
 		->update("far", far)
 		->update("numSliceMaps", 4)
-		->texture("bitMask", bitmask)
+		->texture("bitMask", bitmask) // does this even work?
 		->run();
 
 		// render scene
