b5209dd97c0ecc80934b94e4a29e0e1827a6ece1
BatsiMcLovin <kreuzgar@yahoo.de>
2015-02-01 18:50:54 +0100

    rle now red but working somehow

--- src/executables/Test_Compression/testCMPRSSN.cpp
+++ src/executables/Test_Compression/testCMPRSSN.cpp
@@ -128,7 +128,7 @@ vector<ColorField*> doRLE2(float *array){
 	ColorField *temp = new ColorField(count, rOld, gOld, bOld, aOld);
 	data.push_back(temp);
 
-	cout << "array had: " << i << endl;
+//	cout << "array had: " << i << endl;
 
 	return data;
 }
@@ -183,7 +183,37 @@ void doRLEDecode2(vector<ColorField*> data, float* array){
 		data.pop_back();
 	}
 
-	cout << "array has now: " << i << " Entries" << endl;
+//	cout << "array has now: " << i << " Entries" << endl;
+}
+
+void doRLEDecode3(vector<ColorField*> data, float* array){
+	int count;
+	int i = 0;
+
+	for (ColorField* c : data){
+		count = data.back()->appearence;
+		if (count == 1){
+			array[i] = data.back()->r;
+			array[i + 4] = data.back()->b;
+			array[i + 8] = data.back()->g;
+			array[i + 12] = data.back()->a;
+
+			i += 16;
+		}
+		else{
+			for(int j = 0; j < count; j++){
+				array[i] = data.back()->r;
+				array[i + 4] = data.back()->b;
+				array[i + 8] = data.back()->g;
+				array[i + 12] = data.back()->a;
+				i += 16;
+			}
+		}
+
+		data.pop_back();
+	}
+
+//	cout << "array has now: " << i << " Entries" << endl;
 }
 
 double calculateFPS(double interval = 1.0 , std::string title = "NONE"){
@@ -318,18 +348,18 @@ int main(int argc, char *argv[]) {
         -> texture("tex2", textureHandle)
         -> run();
 
-        RGBtoYCbCr->use();
-        glBindImageTexture(0, pass->get("fragColor"), 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);		//INPUT texture
-        glBindImageTexture(1, tex1Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);				//OUTPUT texture
-        glDispatchCompute(int(width/16), int(height/16), 1);
-        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+//        RGBtoYCbCr->use();
+//        glBindImageTexture(0, pass->get("fragColor"), 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);		//INPUT texture
+//        glBindImageTexture(1, tex1Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);				//OUTPUT texture
+//        glDispatchCompute(int(width/16), int(height/16), 1);
+//        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
-        compressCbCr->use();
-        glBindImageTexture(0, tex1Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);					//INPUT texture
-        glBindImageTexture(1, tex3Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RG32F);					//OUTPUT texture1  Chroma-Channels (Cb, Cr)
-        glBindImageTexture(2, tex4Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RG32F);					//OUTPUT texture2  Brightness-Channel (Y) and Depth-Channel/transperancy (A)
-        glDispatchCompute(int(width/16), int(height/16), 1);
-        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+//        compressCbCr->use();
+//        glBindImageTexture(0, tex1Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);					//INPUT texture
+//        glBindImageTexture(1, tex3Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RG32F);					//OUTPUT texture1  Chroma-Channels (Cb, Cr)
+//        glBindImageTexture(2, tex4Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RG32F);					//OUTPUT texture2  Brightness-Channel (Y) and Depth-Channel/transperancy (A)
+//        glDispatchCompute(int(width/16), int(height/16), 1);
+//        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
 //        YCbCrToRGB->use();
 //        glBindImageTexture(0, tex1Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
@@ -337,14 +367,14 @@ int main(int argc, char *argv[]) {
 //        glDispatchCompute(int(width/16), int(height/16), 1);
 //        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
-        compressedYCbCrToRGB->use();
-        glBindImageTexture(0, tex3Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RG32F);					//INPUT texture1  Chroma-Channels (Cb, Cr)
-        glBindImageTexture(1, tex4Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RG32F);					//INPUT texture2  Brightness-Channel (Y) and Depth-Channel (A)
-        glBindImageTexture(2, tex2Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);				//OUTPUT texture RGBA
-        glDispatchCompute(int(width/16), int(height/16), 1);
-        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+//        compressedYCbCrToRGB->use();
+//        glBindImageTexture(0, tex3Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RG32F);					//INPUT texture1  Chroma-Channels (Cb, Cr)
+//        glBindImageTexture(1, tex4Handle, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RG32F);					//INPUT texture2  Brightness-Channel (Y) and Depth-Channel (A)
+//        glBindImageTexture(2, tex2Handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);				//OUTPUT texture RGBA
+//        glDispatchCompute(int(width/16), int(height/16), 1);
+//        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
-        glBindTexture(GL_TEXTURE_2D, tex2Handle);
+        glBindTexture(GL_TEXTURE_2D, pass->get("fragColor"));
         glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_FLOAT, data);
         glBindTexture(GL_TEXTURE_2D, 0);
 
@@ -355,7 +385,7 @@ int main(int argc, char *argv[]) {
 
 //        double thisTime = glfwGetTime();
 
-        doRLEDecode2(test, data2);
+        doRLEDecode3(test, data2);
 
         for (ColorField* x : test){
      	   delete x;
@@ -374,8 +404,8 @@ int main(int argc, char *argv[]) {
 
         pass2																			//show on a plane
         ->clear(1, 1, 1, 0)
-        ->texture("tex2", pass->get("fragColor"))
-//        ->texture("tex2", tex2Handle)
+//        ->texture("tex2", pass->get("fragColor"))
+        ->texture("tex2", tex2Handle)
         ->run();
 //        int wiggle;
 //        glBindTexture(GL_TEXTURE_2D, tex3Handle);
