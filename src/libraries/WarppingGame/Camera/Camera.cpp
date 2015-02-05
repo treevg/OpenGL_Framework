cbd74c2c1b82d659cd44531461168ffb1e979770
Ivanna07 <ivannamyckhal@uni-koblenz.de>
2015-02-05 22:58:23 +0100

    improved camera Class

--- src/libraries/WarppingGame/Camera/Camera.cpp
+++ src/libraries/WarppingGame/Camera/Camera.cpp
@@ -1,4 +1,5 @@
 #include "Camera.h"
+#include <cmath>
 
 //helpers
 
@@ -11,20 +12,105 @@ vec3 c_view;
 vec3 c_upVector;
 vec3 c_strafeVector; */
 
-
+/* initial position of camera*/
 Camera::Camera(){
+
+this->c_position = vec3(0.0, 0.0, 0.0);
+this->c_view = vec3(0.0, 1.0, 0.5);
+this->c_upVector = vec3(0.0, 0.0, 1.0); 
+this->c_speed = 0.1f;
 
 }
 Camera::~Camera(){}
 
 void Camera::update(){}
 void Camera::lookAt(){}
-void Camera::setCamera(vec3 position, vec3 view, vec3 upVector){}
-void Camera::moveCamera(float speed){}
-void Camera::strafeCamera(float speed){}
-void Camera::rotateAroundObject(float x, float y, float z, vec3 center){}
-void Camera::rotate(float angle, vec3 rotationVector){}
 
+void Camera::setCamera(vec3 position, vec3 view, vec3 upVector){
+
+this->c_position = position;
+this->c_view = view;
+this->c_upVector = upVector; 
+
+
+}
+
+
+static float lengthOfVector(vec3 vector){
+
+return (float) sqrt((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z));
+
+}
+
+
+
+static vec3 calculateCrossProd(vec3 vec1, vec3 vec2){
+
+vec3 result;
+
+result.x = ((vec1.y * vec2.z) - (vec1.z * vec2.y));
+result.y = ((vec1.z * vec2.x) - (vec1.x * vec2.z));
+result.z = ((vec1.x * vec2.y) - (vec1.y * vec2.x));
+
+return result;
+
+
+}
+
+void Camera::moveCamera(float speed){
+
+vec3 direction = this->c_view - this->c_position;
+//normilize direction_vector
+direction = direction/lengthOfVector(direction);
+
+//accelration to  new position
+this->c_position.x = this->c_position.x + direction.x*this->c_speed;
+this->c_position.z = this->c_position.z + direction.z*this->c_speed;
+//acceleration to view
+
+this->c_view.x = this->c_view.x + direction.x*this->c_speed;
+this->c_view.z = this->c_view.z + direction.z*this->c_speed;
+
+
+}
+
+/* approch and code for this funktion from http://www.tomdalling.com/blog/modern-opengl/04-cameras-vectors-and-input/ */
+
+void Camera::strafeCamera(float speed){}
+
+
+void Camera::rotateAroundObject(float x, float y, float z, vec3 center){}
+
+
+/* approch and code for this funktion from http://www.tomdalling.com/blog/modern-opengl/04-cameras-vectors-and-input/ */
+
+void Camera::rotate(float angle, vec3 rotationVector){
+
+   //direction we are looking
+
+   vec3 direction = c_view - c_position;
+
+   float cosAlpha = (float) cos(angle);
+   float sinAlpha = (float) sin (angle);
+   vec3 nView;
+
+    nView.x  = (cosAlpha + (1 - cosAlpha) * rotationVector.x * rotationVector.x)	* direction.x;
+	nView.x += ((1 - cosAlpha) * rotationVector.x * rotationVector.y - rotationVector.z * sinAlpha)	* direction.y;
+	nView.x += ((1 - cosAlpha) * rotationVector.x * rotationVector.z + rotationVector.y * sinAlpha)	* direction.z;
+	nView.y  = ((1 - cosAlpha) * rotationVector.x * rotationVector.y + rotationVector.z * sinAlpha)	* direction.x;
+	nView.y += (cosAlpha + (1 - cosAlpha) * rotationVector.y * rotationVector.y) * direction.y;
+	nView.y += ((1 - cosAlpha) * rotationVector.y * rotationVector.z - rotationVector.x * sinAlpha)	* direction.z;
+	nView.z  = ((1 - cosAlpha) * rotationVector.x * rotationVector.z - rotationVector.y * sinAlpha)	* direction.x;
+	nView.z += ((1 - cosAlpha) * rotationVector.y * rotationVector.z + rotationVector.x * sinAlpha)	* direction.y;
+	nView.z += (cosAlpha + (1 - cosAlpha) * rotationVector.z * rotationVector.z) * direction.z;
+
+	c_view = c_position + nView;
+
+}
+
+
+
+void Camera::moveWithKeybord(){}
 
 
 
@@ -47,6 +133,11 @@ vec3 Camera::getStrafeVector() const{
 
 }
 
+float Camera::getSpeed() const{
+return c_speed;
+
+}
+
 void Camera::setPosition(vec3 position){
 
 
@@ -54,13 +145,23 @@ void Camera::setPosition(vec3 position){
 void Camera::setView(vec3 view){
 
 }
+
 void Camera::setUpVector(vec3 upVector){
 
 }
+
+
 void Camera::getStrafeVector(vec3 strafeVector){
 
+}
+
+void Camera::setSpeed(float speed){
+	this->c_speed=speed;
 
 }
+
+
+/* helpers */
 
 
 
--- src/libraries/WarppingGame/Camera/Camera.h
+++ src/libraries/WarppingGame/Camera/Camera.h
@@ -19,9 +19,9 @@ class Camera {
 
 public:
 
-    //default Camera in FirstPersonPerspective 
+//default Camera in FirstPersonPerspective  -> to implement 
 
-	Camera(CameraMode mode);
+Camera(CameraMode mode);
 
 Camera();
 ~Camera();
@@ -33,6 +33,7 @@ void moveCamera(float speed);
 void strafeCamera(float speed);
 void rotateAroundObject(float x, float y, float z, vec3 center);
 void rotate(float angle, vec3 rotationVector);
+void moveWithKeybord();
 
 /* getters and setters */
 
@@ -41,12 +42,14 @@ vec3 getPosition() const;
 vec3 getView() const;
 vec3 getUpVector() const;
 vec3 getStrafeVector() const;
+float getSpeed()const;
 
 
 void setPosition(vec3 position);
 void setView(vec3 view);
 void setUpVector(vec3 upVector);
 void getStrafeVector(vec3 strafeVector);
+void setSpeed(float speed);
 
 
 protected:
@@ -55,11 +58,11 @@ vec3 c_position;
 vec3 c_view;
 vec3 c_upVector;
 vec3 c_strafeVector;
-
-
-
+float c_speed;
    
 
 };
+
+
 
 #endif // CAMERA_H
