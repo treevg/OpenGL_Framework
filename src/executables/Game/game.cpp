922938ca940da3c618965dba2236998f054c7dc0
Gerrit <treevg@gmail.com>
2015-02-17 15:54:02 +0100

    latency

--- src/executables/Game/game.cpp
+++ src/executables/Game/game.cpp
@@ -1,4 +1,4 @@
- 
+#include <queue>
 #include "game.h"
 
 using namespace glm;
@@ -27,6 +27,9 @@ float speed = 0.1f;
  Camera* camera;
  Model* myModel;
  vector<Mesh*> meshes;
+
+std::queue<glm::mat4> latencyQueue;
+int latencyFrameCount = 60;
 
 static void log(ShaderProgram* s){
 
@@ -69,7 +72,7 @@ static void printMatrix(mat4 matrix){
    plane = new RenderPass( new Plane(), sp);
    pyramid  = new RenderPass(  new Pyramid(),  sp);
    camera =  new Camera();
-   myModel = new Model("/home/ivanna/git_repo/OpenGL_Framework/resources/ladybird.obj");
+   myModel = new Model(RESOURCES_PATH "/ladybird.obj");
    meshes = myModel->getMeshes();
    ladybird = new RenderPass(meshes, sp1);
    
@@ -124,13 +127,17 @@ static void  lookAround(){
 
    //render loop for game
 
-    
- 
     renderLoop([]{
      
     glm::mat4 projMat = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 1000.0f);
     glm::mat4 model=glm::mat4(1.0);
     glm::mat4  viewMat= getLookAt();
+
+    latencyQueue.push(viewMat);
+    if (latencyQueue.size() > latencyFrameCount) {
+      latencyQueue.pop();
+    }
+    glm::mat4 viewMatOld = latencyQueue.front();
 
     glm::mat4 modelPyramide;    //= glm::scale(glm::mat4(1.0),glm::vec3(10,40,10));
     glm::mat4 modelS = glm::scale(glm::mat4(1), glm::vec3(10,10,10));
@@ -145,8 +152,8 @@ static void  lookAround(){
     moveWithKeybord();
    
       plane
-        -> clear(0, 0, 0, 0)
-        -> update("uniformView", viewMat)
+        -> clear(0.6, 0.6, 0.8, 0)
+        -> update("uniformView", viewMatOld)
         -> update("uniformModel",model)
         -> update("uniformProjection", projMat)
         -> update("color", glm::vec4(0,1,0,1))
@@ -164,7 +171,7 @@ for (int i = 5; i < 50; i=i+6){
          
 
        pyramid
-        -> update("uniformView", viewMat)
+        -> update("uniformView", viewMatOld)
         -> update("uniformModel",modelPyramide)
         -> update("uniformProjection", projMat)
         -> update("color", glm::vec4(1,0,0,1))
@@ -178,7 +185,7 @@ for (int i = 5; i < 50; i=i+6){
  
         ladybird
         ->  update("uniformModel", modelLadyBird)
-        ->  update("uniformView", viewMat)
+        ->  update("uniformView", viewMatOld)
         ->  update("uniformProjection", projMat)
         ->  update("color", vec4(1,1,0,1))
         ->  runMeshes();
