02d3019d047ae20cf7feaf967f8c07321e558b29
Gerrit <treevg@gmail.com>
2015-04-27 11:13:51 +0200

    thinned

--- src/libraries/Compression/TextureTools.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-namespace TextureTools {
-    GLuint loadTexture(std::string fileName);
-}
--- src/libraries/Compression/stb_image.h
+++
@@ -1,6 +0,0 @@
-#ifndef STBI_HEADER_FILE_ONLY
-#define STBI_HEADER_FILE_ONLY
-
-#include "stb_image.cpp"
-
-#endif   // !STBI_HEADER_FILE_ONLY
